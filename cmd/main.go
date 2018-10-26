c363bfdf9f5a4d5f4a54a02252fb83d937ae35fb
Einar Budsted <budsted@hotmail.com>
2018-10-26 14:14:30 +0200

    started on uirl handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/einarkb/paragliding/server"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, server.Hello(), "r.URL.Path")
-	})
-
 	server := server.Server{}
 	server.Start()
-	http.ListenAndServe(":80", nil)
 }
--- server/server.go
+++ server/server.go
@@ -1,18 +1,50 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+
 	db "github.com/einarkb/paragliding/database"
 )
 
 type Server struct {
 	db *db.DB
+
+	//map request type (eg. GET/POST) that contains map of acceptable urls and the function to handle each url
+	urlHandlers map[string]map[string]func(http.ResponseWriter, *http.Request)
 }
 
+// Start starts the server
 func (server *Server) Start() {
 	server.db = &db.DB{URI: "mongodb://test:test12@ds141783.mlab.com:41783", Name: "a2-trackdb"}
 	server.db.Connect()
+	server.initHandlers()
+
+	http.HandleFunc("/", server.urlHandler)
+	http.ListenAndServe(":80", nil)
 }
 
-func Hello() string {
-	return "hello!"
+func (server *Server) initHandlers() {
+	server.urlHandlers = make(map[string]map[string]func(http.ResponseWriter, *http.Request))
+	server.urlHandlers["GET"]["/test/"] = handleTest
+}
+
+func handleTest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello!!!!")
+}
+
+func (server *Server) urlHandler(w http.ResponseWriter, r *http.Request) {
+	handlerMap, exists := server.urlHandlers[r.Method]
+	if !exists {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	for url, hFunc := range handlerMap {
+		fmt.Fprint(w, r.URL.Path)
+		if r.URL.Path == url {
+			hFunc(w, r)
+			return
+		}
+	}
+
 }
