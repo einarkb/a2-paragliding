423fce435534fc45ddf6df2577f6d1d961349825
Einar Budsted <budsted@hotmail.com>
2018-10-27 14:08:19 +0200

    get all ids

--- database/db.go
+++ database/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/bson/objectid"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
@@ -15,6 +17,16 @@ type DB struct {
 
 	conn *mongo.Client
 	db   *mongo.Database
+}
+
+type TrackInfo struct {
+	ID          objectid.ObjectID `bson:"_id" json:"-"`
+	HDate       string            `bson:"H_date" json:"H_Date"`
+	Pilot       string            `bson:"pilot" json:"pilot"`
+	Glider      string            `bson:"glider" json:"glider"`
+	GliderID    string            `bson:"glider_id" json:"glider_id"`
+	TrackLength string            `bson:"track_length" json:"track_length"`
+	TrackURL    string            `bson:"track_url" json:"track_url"`
 }
 
 // Connect creates a connection to the database
@@ -36,4 +48,26 @@ func (db *DB) Insert(collection string, obj interface{}) (string, bool) {
 		return "", false
 	}
 	return res.InsertedID.(*bson.Element).Value().ObjectID().Hex(), true
+}
+
+// GetAllTrackIDs returns an array of all the track ids in the database
+func (db *DB) GetAllTrackIDs() ([]objectid.ObjectID, error) {
+	var cursor mongo.Cursor
+	var err error
+	cursor, err = db.db.Collection("tracks").Find(context.Background(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+	var ids []objectid.ObjectID
+	track := TrackInfo{}
+	for cursor.Next(context.Background()) {
+		err := cursor.Decode(&track)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ids = append(ids, track.ID)
+	}
+	return ids, err
 }
--- track/track.go
+++ track/track.go
@@ -16,16 +16,6 @@ type TrackMgr struct {
 	DB *db.DB
 }
 
-type TrackInfo struct {
-	ID          objectid.ObjectID `bson:"_id" json:"-"`
-	HDate       string            `bson:"H_date" json:"H_Date"`
-	Pilot       string            `bson:"pilot" json:"pilot"`
-	Glider      string            `bson:"glider" json:"glider"`
-	GliderID    string            `bson:"glider_id" json:"glider_id"`
-	TrackLength string            `bson:"track_length" json:"track_length"`
-	TrackURL    string            `bson:"track_url" json:"track_url"`
-}
-
 // HandlerPostTrack is the handler for POST /api/track. it registers the track and replies with the id
 func (tMgr *TrackMgr) HandlerPostTrack(w http.ResponseWriter, r *http.Request) {
 	var postData map[string]string
@@ -58,6 +48,12 @@ func (tMgr *TrackMgr) HandlerPostTrack(w http.ResponseWriter, r *http.Request) {
 // HandlerGetAllTracks is the handler for GET /api/track. it replies with an array of all track ids
 func (tMgr *TrackMgr) HandlerGetAllTracks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "application/json")
+	ids, err := tMgr.DB.GetAllTrackIDs()
+	if err != nil {
+		http.Error(w, "Could not receive track list", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(ids)
 
 }
 
