0bd18bc0aca2d3f6338b4743affcf1ba67959a16
Einar Budsted <budsted@hotmail.com>
2018-10-27 16:04:06 +0200

    debugging

--- database/db.go
+++ database/db.go
@@ -89,9 +89,9 @@ func (db *DB) GetTrackByID(id string) (TrackInfo, bool) {
 			log.Fatal(err)
 		}
 	}
-	if track == (TrackInfo{}) {
+	/*if track == (TrackInfo{}) {
 		return track, false
-	}
+	}*/
 
 	return track, true
 }
